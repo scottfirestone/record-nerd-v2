0cb128635d8d0ca1fb8fea41c48384dafd9615d2
Scott Firestone <scott.firestone.dev@gmail.com>
2016-08-24 12:11:04 -0600

    Import components from last version of project
    Install superagent

---
+++ src/components/UserInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserInfo extends React.Component{
+  render() {
+    return(
+      <div>
+        Hello World UserPage
+      </div>
+    )
+  }
+}
+
+export default UserInfo;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './index.css';
 import React from 'react';
 import { render } from 'react-dom';
+import LandingPage from './components/LandingPage'
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
         <p>Global bootstrap css import works too as you can see on the following button.</p>
         <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
+        <LandingPage />
       </div>
     )
   }
